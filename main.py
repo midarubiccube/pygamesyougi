809fb2a478955c6e3150252e9d3d2e5e6ec34ecf
midarubiccube <mi.dai.tomo@gmail.com>
2021-01-13 21:26:11 +0900

    変更

--- main.py
+++ main.py
@@ -2,33 +2,29 @@ import pygame
 from pygame.locals import *
 import sys
 from koma import Komaclass
-from draw import draw_startstage
+from startstage import startstage
 
 x, y = 800, 800
 
 koma_route = ["ginn", "ginnnaru", "gyoku", "hisya", "hisyanaru", "ho", "honaru", "kaku", "kakunaru", "keima", "keimanaru", "kinn", "kyousya", "kyousyanaru", "ou"]
 SCR_RECT = Rect(0, 0, 600, 400)
 
-def main():
+#初期化する
+def init():
     pygame.init() # 初期化
-    screen = pygame.display.set_mode((600, 400))
+    screen = pygame.display.set_mode(SCR_RECT.size)
     pygame.display.set_caption("Pygame Test")
-    icom = pygame.image.load("asset/icon.png")
-    pygame.display.set_icon(icom)
-    press_space = True
-    while(press_space):
-        draw_startstage(screen, SCR_RECT)
-        pygame.display.update()
-        for event in pygame.event.get(): # 終了処理
-            if  event.type == KEYDOWN and event.key == K_SPACE:
-                press_space = False
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+    pygame.display.set_icon(pygame.image.load("asset/icon.png"))
+    startstage(screen, SCR_RECT)
+    return screen
+
+def main():
+    screen = init()
     bg = pygame.image.load("asset/back.jpg").convert_alpha()
     rect_bg = bg.get_rect()
     koma_group = pygame.sprite.Group()
-
+    for i in range(10):
+        koma_group.add(Komaclass(koma_route[5], "ho"))
     while(True):
         screen.fill((255, 255, 255, 0)) # 背景色の指定。RGBのはず
         screen.blit(bg, rect_bg) # 背景画像の描画
@@ -48,11 +44,7 @@ if __name__ == "__main__":
     main()
 
 def koma_img_load():
-    kona_sprites = pygame.sprite.Group()
-    for i in range(10):
-        all_sprites.add(Komaclass(koma_route[5], "ho"))
-    return ho_list
-    
+    y
 
 print(koma_img_load())
 
